06bc96dede4cc450d06ef30273041f4409eaabda
lly001940 <l190543@outlook.com>
2023-10-31 22:20:41 +0800

    chuilishi_help

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <div></div>
-    <div></div>
-    <div></div>
+    <div class="box1">
+      备用盒子
+    </div>
+    <div class="box2">
+      放echarts组件，实现数据可视化
+    </div>
   </div>
 </template>
 
@@ -14,9 +17,16 @@ name:'MyIndex'
 
 <style scoped>
 .container {
-  
+  display: flex;
   background-color: #E9EEF3;
   height: 100%;
-  padding: 18px;
+  padding: 10px;
+}
+.container > div {
+  width: 50%;
+  height: 90%;
+  padding: 10px;
+  margin: 10px;
+  background-color: beige;
 }
 </style>
